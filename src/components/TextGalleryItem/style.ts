90a384860bb9cee5dd2406cd6b24b8ca03fe219d
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2025-01-25 22:44:26 +0000

    refactor: reorganizes code

---
+++ src/components/TextGalleryItem/style.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const CustomContainer = styled.div<{
+    $gridRowStart: number;
+    $gridRowEnd: number;
+    $gridColumnStart: number;
+    $gridColumnEnd: number;
+  }>`
+    width: 100%;
+    height: 100%;
+    grid-row: ${(props) => `${props.$gridRowStart}/${props.$gridRowEnd}`};
+    grid-column: ${(props) =>
+      `${props.$gridColumnStart}/${props.$gridColumnEnd}`};
+`;
+  
+export const MainContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 20%;
+    height: 100%;
+    gap: 1.6em;
+`;
+  
+export const Title = styled.p`
+    position: relative;
+    font-weight: bolder;
+    font-size: 2.5em;
+    &::before {
+      content: "";
+      position: absolute;
+      top: -10px;
+      left: 0;
+      height: 15px;
+      width: 30%;
+      background-color: green;
+    }
+`;
+  
+export const Description = styled.p`
+    font-size: 1.2em;
+`;
+  
+export const Button = styled.button`
+    padding: 0.7em 1.2em;
+    text-align: center;
+    background: none;
+    border: 1px solid black;
+    box-shadow: 0px 0px 20px -8px rgba(0, 0, 0, 0.75);
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+  
+    &:hover {
+      transform: translateY(-5px);
+    }
+`;
+  
---
+++ src/components/TextGalleryItem/type.ts
@@ -0,0 +1,10 @@
+export type TextItemProps = {
+    title: string;
+    subTitle?: string;
+    buttonText?: string;
+    buttonClick?: () => void;
+    gridRowStart: number;
+    gridRowEnd: number;
+    gridColumnStart: number;
+    gridColumnEnd: number;
+};
