5db454cead3730a0e11687c597596bca2b0c3359
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2025-01-26 15:59:42 +0000

    refactor: redo modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useRef } from "react";
 import { StyledDialog, CloseButton } from "./style";
 import { ModalProps } from "./type";
 
 export const Modal = ({ children, isOpen, onClose }: ModalProps) => {
-  const modalRef = useRef<HTMLDialogElement>(null);
-
-  useEffect(() => {
-    const modalElement = modalRef.current;
-    if (!modalElement) return;
-
-    if (isOpen) {
-      modalElement.showModal();
-    } else {
-      modalElement.close();
-    }
-  }, [isOpen]);
-
   return (
-    <StyledDialog ref={modalRef}>
+    <StyledDialog open={isOpen}>
       <CloseButton className="modal-close-btn" onClick={onClose}>
         Close
       </CloseButton>
