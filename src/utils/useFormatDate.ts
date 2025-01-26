a19264b07b042171567a4919730386fe108d39d2
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2025-01-26 21:31:14 +0000

    fix: remove hours from date

--- src/utils/useFormatDate.ts
+++ src/utils/useFormatDate.ts
@@ -2,10 +2,9 @@ export const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
 
     const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Mês começa em 0
+    const month = String(date.getMonth() + 1).padStart(2, "0"); 
     const year = String(date.getFullYear()).slice(-2);
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
 
-    return `${day}/${month}/${year} - ${hours}:${minutes}`;
+
+    return `${day}/${month}/${year}`;
   };
